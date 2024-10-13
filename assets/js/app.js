d56ce8b41c25bfa3823a6d0efe970b99636560f5
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-10-13 07:46:25 +0000

    added delete todo and check mark

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 
@@ -40,4 +41,19 @@ function addTodo(event) {
 
     //clear todoinput value
     todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //Delete todo
+    if(item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    //Check mark
+    if(item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
