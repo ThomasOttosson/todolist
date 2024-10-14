cc0eac406462ed868d143c20ec08693bb1d9de45
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-10-14 11:21:20 +0000

    adeed local storage function

--- assets/js/app.js
+++ assets/js/app.js
@@ -91,3 +91,26 @@ function filterTodo(e) {
         }
     });
 }
+
+function saveLocalTodos(todo) {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos() {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+        
+    });
+}
