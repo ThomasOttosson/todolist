0ece72b3055f8581398b6fb03cfc3248745f28cc
Thomas Ottosson <tomasottosson1@gmail.com>
2024-10-20 10:19:57 +0000

    added more javascript to the timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -170,6 +170,8 @@ function start() {
 
     if(!isRunning) {
         startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
     }
 }
 
@@ -177,4 +179,19 @@ function stop() {}
 
 function reset() {}
 
-function update() {}
+function update() {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
