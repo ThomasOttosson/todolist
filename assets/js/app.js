7460dce0f2dae029a3dfdf02234ae1a2ec75c600
Thomas Ottosson <tomasottosson1@gmail.com>
2024-10-20 10:34:43 +0000

    added the final js code of the timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -175,9 +175,23 @@ function start() {
     }
 }
 
-function stop() {}
+function stop() {
 
-function reset() {}
+    if(isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function reset() {
+
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
 
 function update() {
     const currentTime = Date.now();
